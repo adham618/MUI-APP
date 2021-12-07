4fe48ee2c584988eb1c7686b7b3c14f5a0acaa48
adham618 <adhamtarek291@gmail.com>
2021-12-07 21:33:26 +0200

    custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Notes from "./components/Notes"
 import Create from "./components/Create"
+import { createTheme, ThemeProvider } from "@mui/material";
+import { purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fefefe"
+    },
+    secondary: purple
+  }
+})
+
 function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Notes />} />
-          <Route path="/create" element={<Create />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Notes />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </div>
   )
 }
