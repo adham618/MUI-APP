8f356df3c691a1dcffbb769c5c35148dd3ea676a
adham618 <adhamtarek291@gmail.com>
2021-12-08 18:28:58 +0200

    handleDelete

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,5 +1,6 @@
 import { Container, Grid, Paper } from "@mui/material"
 import { useEffect, useState } from "react"
+import NoteCard from "../components/NoteCard"
 
 const Notes = () => {
   const [notes, setNotes] = useState([])
@@ -8,12 +9,21 @@ const Notes = () => {
       .then(res => res.json())
       .then(data => setNotes(data))
   }, [])
+
+  const handleDelete = async (id) => {
+    await fetch('http://localhost:8000/notes/' + id, {
+      method: 'DELETE'
+    })
+    const newNotes = notes.filter(note => note.id != id)
+    setNotes(newNotes)
+  }
+
   return (
     <Container>
-      <Grid container>
+      <Grid container spacing={3}>
         {notes.map(note => (
           <Grid item key={note.id} xs={12} md={6} lg={4}>
-            <Paper>{note.title}</Paper>
+            <NoteCard note={note} handleDelete={handleDelete} />
           </Grid>
         ))}
       </Grid>
