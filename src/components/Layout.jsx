77f8916e60a8cf047d978f92c065ca64de4b49d0
adham618 <adhamtarek291@gmail.com>
2021-12-09 14:44:17 +0200

    drawer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,46 @@
+import { Drawer, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+const drawerWidth = 240
 const Layout = ({ children }) => {
+  const classes = {
+    page: {
+      background: '#f9f9f9',
+      width: '100%',
+    },
+    drawer: {
+      width: drawerWidth,
+      ".MuiDrawer-paper": {
+        width: drawerWidth,
+      },
+    },
+    root: {
+      display: 'flex'
+    }
+  }
   return (
-    <div>
-      <div sx={{ background: "#f9f", width: "100%" }}>
+    <Box sx={classes.root}>
+      {/* app bar */}
+      {/* side drawer */}
+      <Drawer
+        sx={classes.drawer}
+        variant="permanent"
+        anchor="left"
+      >
+        <div>
+          <Typography variant="h5">
+            Ninja Note
+          </Typography>
+        </div>
+
+        {/* links/list section */}
+
+      </Drawer>
+      {/* main content */}
+
+      <div sx={classes.page}>
         {children}
       </div>
-    </div>
+    </Box>
   )
 }
 
