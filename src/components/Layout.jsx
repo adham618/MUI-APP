911a59c7143a7482732e881eb8f81505c6cf5eb2
adham618 <adhamtarek291@gmail.com>
2021-12-09 15:23:43 +0200

    list

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,15 @@
 import { Drawer, Typography } from "@mui/material"
 import { Box } from "@mui/system"
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { AddCircleOutlineOutlined, SubjectOutlined } from "@mui/icons-material";
+import { useNavigate, useLocation } from "react-router-dom";
 const drawerWidth = 240
 const Layout = ({ children }) => {
+  const navigate = useNavigate()
+  const location = useLocation()
   const classes = {
     page: {
       background: '#f9f9f9',
@@ -15,8 +23,23 @@ const Layout = ({ children }) => {
     },
     root: {
       display: 'flex'
+    },
+    active: {
+      background: '#f4f4f4'
     }
   }
+  const menuItems = [
+    {
+      text: 'My Notes',
+      icon: <SubjectOutlined color="secondary" />,
+      path: "/"
+    },
+    {
+      text: 'Create Notes',
+      icon: <AddCircleOutlineOutlined color="secondary" />,
+      path: "/create"
+    },
+  ]
   return (
     <Box sx={classes.root}>
       {/* app bar */}
@@ -33,7 +56,19 @@ const Layout = ({ children }) => {
         </div>
 
         {/* links/list section */}
-
+        <List>
+          {menuItems.map(item => (
+            <ListItem
+              button
+              key={item.text}
+              onClick={() => navigate(item.path)}
+              sx={location.pathname == item.path ? classes.active : null}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
       {/* main content */}
 
